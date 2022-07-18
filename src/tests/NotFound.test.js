8880ca4603dfe5690f35c9e8738bbb9c446241a6
Danilo Martins <dansm86@outlook.com>
2022-07-18 18:36:32 -0300

    Req4-Tested the NotFound Component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import userEvent from '@testing-library/user-event';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from './renderWithRouter';
+// import App from '../App';
+
+beforeEach(() => {
+  renderWithRouter(<NotFound />);
+});
+
+describe('testando o componente(page) NotFound', () => {
+  test(`se a página contém um heading h2 com 
+  o texto 'Page requested not found 😭'`, () => {
+    expect(screen
+      .getByRole('heading', { level: 2, name: 'Page requested not found Crying emoji' }))
+      .toBeInTheDocument();
+    expect(screen
+      .getByRole('img', {
+        name: 'Pikachu crying because the page requested was not found',
+      }))
+      .toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
