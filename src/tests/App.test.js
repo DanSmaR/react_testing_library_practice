c6bf486363357082a19b7500bf82491b92e705af
Danilo Martins <dansm86@outlook.com>
2022-07-18 16:45:45 -0300

    Req1-Created test to check the nav links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+beforeEach(() => {
+  renderWithRouter(<App />);
+});
+
+describe('testando o componente App', () => {
+  test(`se o topo da aplicação contém um conjunto
+  fixo de links de navegação`, () => {
+    ['Home', 'Favorite Pokémons', 'About'].forEach((elem) => {
+      expect(screen.getByRole('link', { name: elem }));
+    });
+  });
+});
